74766d5a93f4060d997571ba5082053ed4ddddfd
jazzjack <31614669+auto-jazzjack@users.noreply.github.com>
2021-11-02 22:04:39 +0900

    basic get

---
+++ src/channel/channel.go
@@ -0,0 +1,65 @@
+package Channel
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type Channel struct {
+	client *http.Client
+	port   int
+}
+
+var headers = map[string]interface{}{
+	"Content-Length": nil,
+	"Content-Type":   nil,
+	"Date":           nil,
+}
+
+func NewChannel(port int) *Channel {
+	return &Channel{
+		client: &http.Client{},
+		port:   port,
+	}
+}
+
+func createRequestForUpstream(req *http.Request, port int) *http.Request {
+	var host = strings.Split(req.Host, ":")
+	var newHost = "http://"
+
+	if len(host) != 2 {
+		panic(newHost)
+	} else {
+		newHost += host[0]          //host
+		newHost += ":"              //port
+		newHost += fmt.Sprint(port) //port
+		newHost += req.URL.Path     //path
+		newHost += req.URL.RawQuery
+	}
+
+	var retv, _ = http.NewRequest(req.Method, newHost, req.Body)
+	return retv
+}
+
+func (h Channel) CallRemote(res *http.ResponseWriter, req *http.Request) {
+	var replaced = createRequestForUpstream(req, h.port)
+	var resp, err = h.client.Do(replaced)
+
+	if err != nil {
+		panic(err)
+	} else {
+		(*res).WriteHeader(resp.StatusCode)
+
+		var body, err2 = ioutil.ReadAll(resp.Body)
+		if err2 != nil {
+			panic(err2)
+		}
+
+		for k := range headers {
+			(*res).Header().Add(k, resp.Header.Get(k))
+		}
+		(*res).Write(body)
+	}
+}
---
+++ src/event_loop/event_loop.go
@@ -0,0 +1,22 @@
+package Event_loop
+
+import (
+	"net/http"
+	ch "proxy/src/channel"
+)
+
+type Eventloop struct {
+	channels []*ch.Channel
+	pos      int64
+	size     int64
+}
+
+func NewEventLoop(port int) *Eventloop {
+	return &Eventloop{
+		channels: []*ch.Channel{ch.NewChannel(port)},
+	}
+}
+
+func (el Eventloop) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	el.channels[el.pos].CallRemote(&res, req)
+}
