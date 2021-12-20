6121873afb4ce14b320c72d6eecb9d18fca0de67
kwon <jazzjack@kwonui-MacBookPro.local>
2021-12-20 19:09:44 +0900

    add CountdownLantch

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	ad "proxy/src/admin"
+	"sync"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
@@ -12,12 +13,26 @@ type Channel struct {
 	client *http.Client
 }
 
+var wg = sync.WaitGroup{}
+
 func main() {
+
+	wg.Add(2)
 	fmt.Println("started")
 	var admin = ad.NewAdmin()
 
-	http.Handle("/metrics", promhttp.Handler())
-	http.Handle("/", admin.GetProxy().GetEventLoop())
-	http.ListenAndServe(":9393", nil)
-	//http.ListenAndServe(":9393", admin.GetProxy().GetEventLoop())
+	//admin with other thread
+	go func() {
+		defer wg.Done()
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":9393", nil)
+	}()
+
+	//proxy go
+	go func() {
+		defer wg.Done()
+		http.ListenAndServe(":9393", admin.GetProxy().GetEventLoop())
+	}()
+
+	wg.Wait()
 }
