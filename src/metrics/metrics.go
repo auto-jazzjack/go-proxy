aebe7c937969719c581679e14bfc2e07bfd2eda5
kwon <jazzjack@kwonui-MacBookPro.local>
2021-12-20 22:20:32 +0900

    fix

--- src/metrics/metrics.go
+++ src/metrics/metrics.go
@@ -3,8 +3,6 @@ package Metrics
 import (
 	"time"
 
-	collection "proxy/src/utils"
-
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
@@ -12,66 +10,61 @@ import (
 var buckets = [...]float64{0.5, 0.75, 0.9, 0.95, 0.99}
 
 var emptyMap = map[string]string{}
-var Label2xx = map[string]string{"status": "2xx"}
-var Label3xx = map[string]string{"status": "3xx"}
-var Label4xx = map[string]string{"status": "4xx"}
-var Label5xx = map[string]string{"status": "5xx"}
+
+var LABEL_MAP = map[int]string{2: "2xx", 3: "3xx", 4: "4xx", 5: "5xx"}
 var TOTAL_REQUEST_COUNT = "TOTAL_REQUEST_COUNT"
 var TOTAL_REQUEST_LATENCY = "TOTAL_REQUEST_LATENCY"
+var METRIC_LABEL = []string{"status", "path"}
 
 var cachedMetricObject = make(map[string]GeneralMetric)
 
 type GeneralMetric struct {
-	counter   prometheus.Counter
-	histogram prometheus.Histogram
+	counter   *prometheus.CounterVec
+	histogram *prometheus.HistogramVec
 }
 
 func MeasureCountAndLatency(code int, uri string, from int64) {
 
-	var clonedLabel = emptyMap
-	if 200 <= code && code < 300 {
-		clonedLabel = collection.CopyMap(Label2xx)
-	} else if 300 <= code && code < 400 {
-		clonedLabel = collection.CopyMap(Label3xx)
-	} else if 400 <= code && code < 500 {
-		clonedLabel = collection.CopyMap(Label4xx)
-	} else if 500 <= code && code < 600 {
-		clonedLabel = collection.CopyMap(Label5xx)
-	} else {
-		return
-	}
+	var clonedLabel = []string{}
+
+	clonedLabel = append(clonedLabel, LABEL_MAP[code/100])
+	clonedLabel = append(clonedLabel, uri)
 
 	if _, ok := cachedMetricObject[uri]; !ok {
 		cachedMetricObject[uri] = GeneralMetric{
-			counter:   newCounter(TOTAL_REQUEST_COUNT, clonedLabel),
-			histogram: newHistogram(TOTAL_REQUEST_LATENCY, clonedLabel),
+			counter:   newCounter(TOTAL_REQUEST_COUNT),
+			histogram: newHistogram(TOTAL_REQUEST_LATENCY),
 		}
+	}
+
+	if len(clonedLabel) != 2 {
+		return
 	}
 
 	now := time.Now().UnixMilli()
 	elapsed := float64(now - from)
 
-	cachedMetricObject[uri].counter.Add(1)
-	cachedMetricObject[uri].histogram.Observe(elapsed)
+	cachedMetricObject[uri].counter.WithLabelValues(clonedLabel...).Add(1)
+	cachedMetricObject[uri].histogram.WithLabelValues(clonedLabel...).Observe(elapsed)
 
 }
 
-func newCounter(name string, labels map[string]string) prometheus.Counter {
-	return promauto.NewCounter(
+func newCounter(name string) *prometheus.CounterVec {
+	return promauto.NewCounterVec(
 		prometheus.CounterOpts{
-			Name:        name,
-			ConstLabels: labels,
+			Name: name,
 		},
+		METRIC_LABEL,
 	)
 }
 
-func newHistogram(name string, labels map[string]string) prometheus.Histogram {
+func newHistogram(name string) *prometheus.HistogramVec {
 
-	return promauto.NewHistogram(
+	return promauto.NewHistogramVec(
 		prometheus.HistogramOpts{
-			Name:        name,
-			ConstLabels: labels,
-			Buckets:     buckets[:],
+			Name:    name,
+			Buckets: buckets[:],
 		},
+		METRIC_LABEL,
 	)
 }
